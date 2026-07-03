961e9f4f48e9a576c144fb9794661a015b01d1ca
Mohamed Idris <edriso.dev@gmail.com>
2026-07-03 22:55:50 +0300

    Set router basename for GitHub Pages subpath

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function RouteFallback() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
       <Routes>
         <Route element={<Layout />}>
           <Route
